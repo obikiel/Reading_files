ac6da722ce616a74f8c2eadb26438e944afb4e3b
obioma chijindu <ezekielobiomachi1@gmail.com>
2022-06-07 00:06:55 +0100

    first commit

--- main.py
+++ main.py
@@ -3,14 +3,22 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
-def read_file_content(filename):
-    # [assignment] Add your code here 
-    
-    return "Hello World"
+def readfile(filename):
+    #raeding the file
+    with open("./story.txt", "r") as openfile:
+        read_file = openfile.read()
+    return read_file
 
+def countwords():
+    text = readfile("./story.txt")
+    split_text = text.split()
+    #print(split_text)
+    count = {}
+    for i in split_text:
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+    return count
 
-def count_words():
-    text = read_file_content("./story.txt")
-    # [assignment] Add your code here
-
-    return {"as": 10, "would": 20}
+print(countwords())
